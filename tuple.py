a03b7a448186a06f07e19e4eedf2adba8c88f12d
grance1 <2397580855@qq.com>
2016-10-28 20:31:33 +0800

    update a new content

--- tuple.py
+++ tuple.py
@@ -1,3 +1,24 @@
 #!/user/bin/python2.7
 b=('2','tuple')
 print b
+>>> a_tuple=('a',)
+>>> a_tuple
+('a',)
+>>> a_tuple=('a','b','mpilgrim','z','example')
+>>> a_tuple
+('a', 'b', 'mpilgrim', 'z', 'example')
+>>> x=()
+>>> x
+()
+>>> a=3
+>>> a
+3
+>>> a=(3)
+>>> a
+3
+>>> a=3,
+>>> a
+(3,)
+>>> a=1,2
+>>> a
+(1, 2)
