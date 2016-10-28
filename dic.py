2fb03d45b6053eaf19b73a71964d01f02e1ab764
grance1 <2397580855@qq.com>
2016-10-28 20:48:44 +0800

    this is a dic.py

---
+++ dic.py
@@ -0,0 +1,10 @@
+#!/user/bin/python2.7
+>>> a=(1,2,3,4,5)
+>>> b='hello world.'
+>>> def demo():
+...     a=3
+...     b=[1,2,3]
+...     print('locals:',local())
+...     print('global:',globals())
+...     
+
