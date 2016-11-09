b540c95c714a7608f2c83385fc3acf266e0ffae7
grance1 <2397580855@qq.com>
2016-11-09 21:20:02 +0800

    this is a function.py

---
+++ function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7
+print abs(100)
+print abs(-20)
+print abs(12.34)
+print cmp(1,2)
+print cmp(2,1)
+print cmp(3,3)
+print int('123')
+print int(12.34)
+print str(123)
+print str(1.23)
+from math import fsum
+L = []
+# print L
+L=list(map(lambda x: x*x,range(1,101)))
+print sum(L)
+
+L = range(1,101)
+#print L
+#for x in L:
+print sum(x*x for x in L)
