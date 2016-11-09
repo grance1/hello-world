5760354aec5e7ff86c5397ba87c98dd9069c5d61
grance1 <2397580855@qq.com>
2016-11-09 21:17:29 +0800

    this is a continue.py

---
+++ continue.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2.7
+L = [75,98,59,81,66,43,69,85]
+sum = 0.0
+n = 0
+for x in L:
+    sum = sum + x
+    n = n + 1
+print sum / n
+
+L = [75,98,59,81,66,43,69,85]
+sum = 0.0
+n = 0
+for x in L:
+    if x < 60:
+        continue
+    sum = sum + x
+    n = n + 1
+print sum / n
+
+sum = 0
+x = 0
+while True:
+    x = x + 1
+    if x > 100:
+        break
+    if x % 2 == 0:
+        continue
+    sum = sum + x
+print sum
