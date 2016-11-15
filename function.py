3b92cea89dd40efe6b477a640fa9f9ed25aea298
grance1 <2397580855@qq.com>
2016-11-15 14:48:15 +0800

    update

--- function.py
+++ function.py
@@ -20,3 +20,13 @@ L = range(1,101)
 #print L
 #for x in L:
 print sum(x*x for x in L)
+print abs(100)
+print abs(-20)
+print abs(12.34)
+print cmp(1,2)
+print cmp(2,1)
+print cmp(3,3)
+print int('123')
+print int(12.34)
+print str(123)
+print str(1.23)
