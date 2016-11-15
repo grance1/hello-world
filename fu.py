f2d04a9d0ab0f4d922f61a33d61d3a94fb5b5d1e
grance1 <2397580855@qq.com>
2016-11-15 14:50:33 +0800

    update

--- fu.py
+++ fu.py
@@ -3,4 +3,15 @@
 '123'
 >>> str(1.23)
 '1.23'
+print int('123')
+print int(12.34)
+print str(123)
+print str(1.23)
+print str('1.23')
+from math import fsum
+L = []
+# print L
+L=list(map(lambda x: x*x,range(1,101)))
+print sum(L)
 
+L = range(1,101)
