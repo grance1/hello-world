46b9c30f41815162942b0f93c9c6c2ec041382e0
grance1 <2397580855@qq.com>
2016-11-21 20:24:37 +0800

    this is a object file

---
+++ object.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+std1 = {'name':'Michael','score':98}
+std2 = {'name':'Bob','score':81}
+def print_score(std):
+    print('%s:%s'%(std['name',std['score']]))
+
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+    def print_score(self):
+        print('%s:%s'%(self.name,self.score))
+
+
+
+bart = Student('Bart Simpson',59)
+lisa = Student('Lisa Simpson',87)
+print bart.print_score()
+print lisa.print_score()
