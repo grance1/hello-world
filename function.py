ca4daed9b6d2288d21bd58dcc02015739f6394e5
grance1 <2397580855@qq.com>
2016-11-21 20:34:21 +0800

    add this file

--- function.py
+++ function.py
@@ -34,3 +34,22 @@ def greet(name='world'):
         print 'Hello,'+ name+'.'
 print greet()
 print greet('Bart')
+import math
+def move(x,y,step,angle):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx,ny
+x , y = move(100,100,60,math.pi/6)
+print x , y
+r = move(100,100,60,math.pi/6)
+print r
+
+import math
+def quadratic_equation(a,b,c):
+    if math.sqrt(b *b -4*a*c) >= 0:
+        return (-b+math.sqrt(b*b-4*a*c))/(2*a),(-b-math.sqrt(b*b-4*a*c))/(2*a)
+    else:
+        return "none"
+print quadratic_equation(2,3,1)
+print quadratic_equation(1,3,-4)
+
