2ddb36d92c2e4022ffd53ab08d0d909b8e679ebe
grance1 <2397580855@qq.com>
2016-11-28 14:49:14 +0800

    update this file

--- function.py
+++ function.py
@@ -52,4 +52,19 @@ def quadratic_equation(a,b,c):
         return "none"
 print quadratic_equation(2,3,1)
 print quadratic_equation(1,3,-4)
+>>> str(123)
+'123'
+>>> str(1.23)
+'1.23'
+print int('123')
+print int(12.34)
+print str(123)
+print str(1.23)
+print str('1.23')
+from math import fsum
+L = []
+# print L
+L=list(map(lambda x: x*x,range(1,101)))
+print sum(L)
 
+L = range(1,101)
